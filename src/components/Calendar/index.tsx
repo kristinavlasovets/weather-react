ed7c6aca7d47f430b34307553e3b2b2ee3cd9b9e
Kristina Vlasovets <kristinavlasovets@gmail.com>
2023-03-15 22:27:00 +0300

    started the google calendar api implementation

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,8 +1,35 @@
 import { FC } from "react";
+import ApiCalendar from "react-google-calendar-api";
 
-import { Box, Chip, List, ListItem, ListItemText } from "@mui/material";
+import { Box, Button, Chip, List, ListItem, ListItemText } from "@mui/material";
 
 const Calendar: FC = () => {
+  const config = {
+    clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+    apiKey: process.env.REACT_APP_GOOGLE_CALENDAR_API_KEY,
+    scope: "https://www.googleapis.com/auth/calendar",
+    discoveryDocs: [
+      "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+    ],
+  };
+
+  const apiCalendar = new ApiCalendar(config);
+
+  const handleOnLogin = () => {
+    apiCalendar.handleAuthClick();
+    console.log("click");
+    apiCalendar
+      .listEvents({
+        // timeMin: new Date()..toISOString(),
+        // timeMax: new Date().addDays(10).toISOString(),
+        showDeleted: true,
+        maxResults: 10,
+        orderBy: "updated",
+      })
+      .then(({ result }: any) => {
+        console.log(result.items);
+      });
+  };
   return (
     <Box
       sx={{
@@ -10,6 +37,9 @@ const Calendar: FC = () => {
         width: "100%",
       }}
     >
+      <Button sx={{ p: "20px", color: "white" }} onClick={handleOnLogin}>
+        Check my Google Calendar
+      </Button>
       <List sx={{ pl: "20px", color: "white", fontSize: "32px" }}>
         <ListItem disablePadding>
           <Chip sx={{ color: "white", mr: "10px" }} label="8:00" size="small" />
